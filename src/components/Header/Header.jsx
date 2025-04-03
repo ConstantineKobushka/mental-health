d24318efcb040b60243b15c4b638085799f6e133
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-04-03 15:50:13 +0300

    add header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,32 @@
+import { Link, NavLink } from 'react-router-dom';
+
 import css from './Header.module.css';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className={css.header}>
+      <nav className={css.nav}>
+        <Link className={css.logo} to='/'>
+          <img src='/logo/logo.svg' alt='Logo' />
+        </Link>
+        <div className={css.menu}>
+          <NavLink className={css.menuLink} to='/'>
+            Home
+          </NavLink>
+          <NavLink className={css.menuLink} to='/psychologists'>
+            Psychologists
+          </NavLink>
+          <NavLink className={css.menuLink} to='/favorites'>
+            Favorites
+          </NavLink>
+        </div>
+      </nav>
+      <div className={css.auth}>
+        <button className={css.logIn}>Log In</button>
+        <button className={css.logUp}>Registration</button>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
