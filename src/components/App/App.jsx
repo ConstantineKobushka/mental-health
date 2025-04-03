4d533243883dfe94c4fe2a343d16fa83a7dac363
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-04-03 14:24:43 +0300

    configure routing

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,46 @@
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+const HomePage = lazy(() => import('../../pages/HomePage/HomePage.jsx'));
+const PsychologistsPage = lazy(() =>
+  import('../../pages/PsychologistsPage/PsychologistsPage.jsx')
+);
+const FavoritesPage = lazy(() =>
+  import('../../pages/FavoritesPage/FavoritesPage.jsx')
+);
+const NotFoundPage = lazy(() =>
+  import('../../pages/NotFoundPage/NotFoundPage.jsx')
+);
+
+import Layout from '../Layout/Layout.jsx';
+import RestrictedRoute from '../RestrictedRoute/RestrictedRoute.jsx';
+import PrivateRoute from '../PrivateRoute/PrivateRoute.jsx';
+
 function App() {
-  return <></>;
+  return (
+    <Suspense>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<RestrictedRoute component={<HomePage />} />} />
+
+          <Route
+            path='psychologists'
+            element={<RestrictedRoute component={<PsychologistsPage />} />}
+          />
+
+          <Route
+            path='favorites'
+            element={<PrivateRoute component={<FavoritesPage />} />}
+          />
+
+          <Route
+            path='*'
+            element={<RestrictedRoute component={<NotFoundPage />} />}
+          />
+        </Route>
+      </Routes>
+    </Suspense>
+  );
 }
 
 export default App;
