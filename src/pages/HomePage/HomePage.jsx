883771e1194efdc38e705b1401f9ccd15e8ed478
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-04-03 14:15:42 +0300

    create folders and files structure for the project

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,7 @@
+import css from './HomePage.module.css';
+
+const HomePage = () => {
+  return <div>Home</div>;
+};
+
+export default HomePage;
---
+++ src/pages/PsychologistsPage/PsychologistsPage.jsx
@@ -0,0 +1,7 @@
+import css from './PsychologistsPage.module.css';
+
+const PsychologistsPage = () => {
+  return <div>Psychologists</div>;
+};
+
+export default PsychologistsPage;
