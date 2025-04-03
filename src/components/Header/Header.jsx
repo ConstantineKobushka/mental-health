bc4772f0d3728dba3ad92c42a0171a33fff3c647
Constantine Kobushka <kobushkaconstantine@gmail.com>
2025-04-03 16:31:43 +0300

    add Container component and fixe Header component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,36 @@
 import { Link, NavLink } from 'react-router-dom';
 
+import Container from '../Container/Container';
+
 import css from './Header.module.css';
 
 const Header = () => {
   return (
     <header className={css.header}>
-      <nav className={css.nav}>
-        <Link className={css.logo} to='/'>
-          <img src='/logo/logo.svg' alt='Logo' />
-        </Link>
-        <div className={css.menu}>
-          <NavLink className={css.menuLink} to='/'>
-            Home
-          </NavLink>
-          <NavLink className={css.menuLink} to='/psychologists'>
-            Psychologists
-          </NavLink>
-          <NavLink className={css.menuLink} to='/favorites'>
-            Favorites
-          </NavLink>
+      <Container>
+        <div className={css.headerWrapper}>
+          <nav className={css.nav}>
+            <Link className={css.logo} to='/'>
+              <img src='/logo/logo.svg' alt='Logo' />
+            </Link>
+            <div className={css.menu}>
+              <NavLink className={css.menuLink} to='/'>
+                Home
+              </NavLink>
+              <NavLink className={css.menuLink} to='/psychologists'>
+                Psychologists
+              </NavLink>
+              <NavLink className={css.menuLink} to='/favorites'>
+                Favorites
+              </NavLink>
+            </div>
+          </nav>
+          <div className={css.auth}>
+            <button className={css.logIn}>Log In</button>
+            <button className={css.logUp}>Registration</button>
+          </div>
         </div>
-      </nav>
-      <div className={css.auth}>
-        <button className={css.logIn}>Log In</button>
-        <button className={css.logUp}>Registration</button>
-      </div>
+      </Container>
     </header>
   );
 };
